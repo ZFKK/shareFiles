75996871fadd3a93c73ff82a1faba6e978ecb987
zhengfa <903797097@qq.com>
2016-10-18 20:36:28 +0800

    支付宝支付
    基本完成，审核时间都太长

---
+++ AliPayFirst/AliPayFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AliPayFirst
+//
+//  Created by sunkai on 16/10/18.
+//  Copyright © 2016年 imo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
