9263ed2a32841aa756a14ad5f2e18e8e8cbadb5a
zhengfa <903797097@qq.com>
2016-10-26 11:40:19 +0800

    只是采集和美颜的框架，之后需要一个完整的

---
+++ iOSLiveKitPushStreamTes/iOSLiveKitPushStreamTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSLiveKitPushStreamTest
+//
+//  Created by sunkai on 16/10/26.
+//  Copyright © 2016年 imo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
